5f9557fb50a806d732ed98cd161fa1e4589a9c8e
nanurodriguez <bel17039@byui.edu>
2024-09-23 18:10:50 -0600

    updating css and java script- styiling css and getting last modified in java

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -1,9 +1,9 @@
+
+//adding current year to the page.
 const year = document.querySelector("#year");
-Intl.DateTimeFormat(
-	"en-US",
-	{
-		dateStyle: "full"
-	}
-)
-currentYear.innerHTML = `getFullYear(): span
-class= "highlight">${today.getFullYear()}</span`;
+const today = new Date();
+currentYear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
+
+//Last modified at footer by date:
+alert(document.lastModified);
+let lastModified = new Date(document.lastModified);
