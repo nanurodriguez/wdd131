71c5da43e7f8b89f18a7c2d380dcee5e3dcbd13c
nanurodriguez <bel17039@byui.edu>
2025-03-20 16:18:54 -0600

    finishing menu activity

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,25 +1,36 @@
-
-const input = document.querySelector('scripture');
+const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('list');
+const list = document.querySelector('#list');
+
 
 button.addEventListener('click', function() {
     // Code to execute when the element is clicked
     if (input.value.trim() !== " "){
-        console.log("Please type your favorite scripture");
+        //6.Creating a listScriptures
         const listScriptures = document.createElement('li');
+        //7. create a delete button
         const deleteButton = document.createElement('button');
+        // Create span for scripture text
+        const scriptureText = document.createElement('span'); 
 
-        list.textContent = input.value;
+        //8. Populate the listScriptures variable's textContent
+        scriptureText.textContent = input.value; // Set scripture text
+        //9. button with an x
         deleteButton.textContent = '❌';
-
-        listScriptures.appendChild(deleteButton);//Appends the li element variable with the delete button
+        // Append scripture text span
+        listScriptures.appendChild(scriptureText); 
+        //10. appends the listScriptures with the delete button
+        listScriptures.appendChild(deleteButton);
+        //11.Append the li element variable to the unordered list in your HTML
         list.append(listScriptures);
-            }
         deleteButton.addEventListener('click', function () {
-            list.removeChild(li);
-            input.focus();
-            });
+        list.removeChild(listScriptures);
+        input.focus();
+        });
         input.value = '';
+        input.focus();
+
+        }
+
   });
 
