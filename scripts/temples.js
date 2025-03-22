5ad3c4ca1c6e087ae9ee43faeb0eab891655dea8
nanurodriguez <bel17039@byui.edu>
2025-03-22 15:42:49 -0600

    finishing up CSS and working on Java Script

---
+++ scripts/temples.js
@@ -0,0 +1,24 @@
+//adding current year to the page.
+const year = document.querySelector("#year");
+const today = new Date();
+currentYear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
+
+//Last modified at footer by date:
+const getDate = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = getDate
+
+// Store the selected elements that we are going to use. This is not required but a good practice with larger programs where the variable will be referenced more than once.
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+/* ❔What does toggle mean?
+We could write separate add and remove statements. Toggle adds the class if it does not currently exist or removes the class if it does exist. 
+The CSS class rules will handle the different views, layouts, and displays.
+🗝️ JavaScript only applies the class value or not.
+*/
