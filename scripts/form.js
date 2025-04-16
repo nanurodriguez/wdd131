556c703995ae5e979be40c29cd7f9cdde40ff28d
nanurodriguez <nbell@byupathway.edu>
2025-04-16 10:52:02 -0300

    updating JS of the form

---
+++ scripts/form.js
@@ -0,0 +1,32 @@
+//adding current year to the page.
+const year = document.querySelector("#year");
+const today = new Date();
+currentYear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
+
+//Last modified at footer by date:
+const getDate = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = getDate
+
+
+const products = [
+	{
+	  id: "fc-1888",
+	  name: "Sugar",
+	  averagerating: 4.5
+	},
+	{
+	  id: "fc-2050",
+	  name: "Honey",
+	  averagerating: 4.7
+	},
+	{
+	  id: "fs-1987",
+	  name: "Stevia",
+	  averagerating: 3.5
+	},
+	{
+	  id: "ac-2000",
+	  name: "Monks Fruit",
+	  averagerating: 3.9
+	}
+  ];
