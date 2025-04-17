95b1cfd4d09c0b8045bf4750d2516591abc82832
nanurodriguez <nbell@byupathway.edu>
2025-04-16 22:38:58 -0300

    adding JS

--- project/scripts/website-project.js
+++ project/scripts/website-project.js
@@ -13,3 +13,23 @@ currentYear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`
 //Last modified at footer by date:
 const getDate = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = getDate
+
+//lazy load with IntersectionObserver - image in the H1
+
+document.addEventListener("DOMContentLoaded", function () {
+    const lazyElements = document.querySelectorAll('.lazy-bg');
+  
+    const observer = new IntersectionObserver((entries, obs) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const el = entry.target;
+          const bgImage = el.getAttribute('data-bg');
+          el.style.backgroundImage = `url(${bgImage})`;
+          obs.unobserve(el); // Stop observing once loaded
+        }
+      });
+    });
+  
+    lazyElements.forEach(el => observer.observe(el));
+  });
+  
