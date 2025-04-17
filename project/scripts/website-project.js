5fb18e4c96b69fcc60a413b1b5fdad60cca381ea
nanurodriguez <nbell@byupathway.edu>
2025-04-17 08:40:55 -0300

    finishing up

--- project/scripts/website-project.js
+++ project/scripts/website-project.js
@@ -1,15 +1,11 @@
 // Store the selected elements that we are going to use. This is not required but a good practice with larger programs where the variable will be referenced more than once.
-const mainnav = document.querySelector('.nav-links')
 const hambutton = document.querySelector('.hamburger');
-const logo = document.querySelector('.logo');
+const navLinks = document.querySelector('.nav-links')
 
 // Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
 hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
-	logo.classList.toggle('hide');
-    //let expanded = hambutton.getAttribute('aria-expanded') === 'true' || false;
-    //hambutton.setAttribute('aria-expanded', !expanded);
+  navLinks.classList.toggle('show');
 });
 
 
