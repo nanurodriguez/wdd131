22a5334b6750b6198594bcff7e5af8fb62a3d8b2
nanurodriguez <nbell@byupathway.edu>
2025-04-16 23:01:51 -0300

    updating javascript

--- project/scripts/website-project.js
+++ project/scripts/website-project.js
@@ -1,18 +1,17 @@
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
+// Store the selected elements that we are going to use. This is not required but a good practice with larger programs where the variable will be referenced more than once.
+const mainnav = document.querySelector('.nav-links')
+const hambutton = document.querySelector('.hamburger');
+const logo = document.querySelector('.logo');
 
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('open');
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+	logo.classList.toggle('hide');
+    //let expanded = hambutton.getAttribute('aria-expanded') === 'true' || false;
+    //hambutton.setAttribute('aria-expanded', !expanded);
 });
 
-//adding current year to the page.
-const year = document.querySelector("#currentYear");
-const today = new Date();
-currentYear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
-
-//Last modified at footer by date:
-const getDate = new Date(document.lastModified);
-document.getElementById("lastModified").innerHTML = getDate
 
 //lazy load with IntersectionObserver - image in the H1
 
@@ -32,4 +31,13 @@ document.addEventListener("DOMContentLoaded", function () {
   
     lazyElements.forEach(el => observer.observe(el));
   });
+
+  //adding current year to the page.
+const year = document.querySelector("#currentYear");
+const today = new Date();
+currentYear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
+
+//Last modified at footer by date:
+const getDate = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = getDate
   
